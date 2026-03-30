988f93dca25a70ff1ff94d6fa1b203d5a93bdddb
Kamil_hyperbook <k.marczak@savicon.pl>
2026-03-30 10:40:49 +0200

    Added BusinessLogic to Services

---
+++ APBD_project1/Services/BusinessLogic.cs
@@ -0,0 +1,27 @@
+﻿using APBD_project1.Models;
+
+namespace APBD_project1.Services;
+
+public class BusinessLogic
+{
+    private List<Device> _device = new();
+    private List<User> _users = new();
+    private List<Rental> _rentals = new();
+
+    public void AddEquipment(Device device)
+    {
+        _device.Add(device);
+    }
+
+    public void AddUser(User user)
+    {
+        _users.Add(user);
+    }
+
+    public List<Device> GetAvailableEquipment()
+    {
+        return _device.Where(e => e.IsAvailable).ToList();
+    }
+
+    
+}
